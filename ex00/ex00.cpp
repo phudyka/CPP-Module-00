e2b449578e49790886716d2b158bf0d3779f0bec
phudyka <paul.hudyka@gmail.com>
2023-10-23 13:11:42 +0200

    ex00

--- ex00/ex00.cpp
+++ ex00/ex00.cpp
@@ -6,14 +6,32 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:48:54 by phudyka           #+#    #+#             */
-/*   Updated: 2023/10/23 11:49:25 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/10/23 13:10:17 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
 #include <iostream>
 
-int	main()
+using namespace std;
+
+int	main(int argc, char **argv)
 {
-	
+	string	caps;
+
+	if (argc > 1)
+	{
+		for (int i = 1; argv[i]; i++)
+			{
+				for(int j = 0; argv[i][j]; j++)
+				{
+					caps = toupper(argv[i][j]);
+					cout << caps;
+				}
+			}
+		cout << endl;
+	}
+	else
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
 	return (0);
 }
